12c822a4e716ca6290880d97b1d9942c32a35923
TobiCrackIT <oluwatoyinoluwatobig@gmail.com>
2023-01-16 12:00:41 +0100

    complete NFT contract

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -11,3 +11,7 @@ contract NFTee is ERC721 {
     }
 
 }
+
+//0x90884E5E21AdEB92aedee0b91678Abb935912b84
+//0x899351f150CA24E7150118bb87D7af9552B1678B
+//npx hardhat run scripts/deploy.js --network goerli
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({path: ".env"});
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks:{
+    goerli : {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts:[process.env.PRIVATE_KEY]
+    }
+  }
 };
